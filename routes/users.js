ee29d8c57605fe17d8c8d8fbb36e6a841723b4b2
crovax124 <eric.isensee11@gmail.com>
2021-12-06 10:44:22 +0100

    used other multer functionalities

---
+++ routes/users.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const multer = require('multer');
+
+const upload = multer({dest: 'images'});
+
+const router = express.Router();
+
+router.get('/', function(req, res) {
+  res.render('profiles');
+});
+
+router.get('/new-user', function(req, res) {
+  res.render('new-user');
+});
+
+router.post('/profiles', upload.single('image'), async function(req, res) {
+const uploadedImageFile = req.file;
+const userData = req.body;
+
+res.redirect('/')
+});
+
+module.exports = router;
